2cdde82b98b383665e82efdc3f013ece217adfc1
bmeinert8 <brian.meinert1@gmail.com>
2025-01-03 12:51:15 -0500

    created function to save the cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
-  productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+//function to save cart to local storage
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 //function to add product to the cart array
 export function addToCart(productId) {
@@ -24,6 +33,8 @@ export function addToCart(productId) {
       quantity
     });
   }
+
+  saveToStorage();
 }
 
 //function to remove product from the cart array
@@ -36,4 +47,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
