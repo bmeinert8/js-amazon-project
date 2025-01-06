326a603baeb9720d9eeb3fdb2c8bf2a151985d21
bmeinert8 <brian.meinert1@gmail.com>
2025-01-05 20:48:34 -0500

    Refactor cart quantity update logic and ensure it reflects after item deletion

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -104,14 +104,25 @@ document.querySelectorAll('.js-delete-link')
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      updateCartQuantity();
     });
   });
 
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+    
+    cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
-  });
-  
-  document.querySelector('.js-return-to-home-link')
+    });
+
+    document.querySelector('.js-return-to-home-link')
     .innerHTML = `${cartQuantity} items`;
+  }
+
+  updateCartQuantity();
+  
+
+  
+  
+  
