705beee78a0fea3d6222487ca23765c1c93b9cf9
bmeinert8 <brian.meinert1@gmail.com>
2025-01-09 09:52:19 -0500

    update order summary rendering after delivery option selection

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -178,6 +178,7 @@ function renderOrderSummary() {
     element.addEventListener('click', () => {
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary();
     });
   });
 
