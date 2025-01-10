de0524bb3fc78a4909717dd9c7eb370c67b554da
bmeinert8 <brian.meinert1@gmail.com>
2025-01-09 20:52:57 -0500

    refactor payment summary to dynamically calculate item quantity and clean up import statements

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -9,7 +9,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
-import { renderPaymentSummary } from './paymentSummary.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 
 //function to rewrite the order summary anytime something changes in the cart.
