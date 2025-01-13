18e5993b174bc40d48580fadd67b7cfa180bfab3
bmeinert8 <brian.meinert1@gmail.com>
2025-01-13 10:23:34 -0500

    create automated tests for add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-  cart = [{
-    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
 
 //function to save cart to local storage
@@ -20,23 +26,24 @@ function saveToStorage() {
 //function to add product to the cart array
 export function addToCart(productId) {
   let matchingItem;
+
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
+
   const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
   let quantity = Number(quantitySelector.value);
   if (matchingItem) {
     matchingItem.quantity += quantity;
-  } else{
+  } else {
     cart.push({
-      productId,
-      quantity,
+      productId: productId,
+      quantity: quantity,
       deliveryOptionId: '1'
     });
   }
-
     saveToStorage();
 }
 
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 /*describe is a function that takes two arguments: a string and a function that contains the test suite*/
 describe('test suite: formatCurrency', () => {
