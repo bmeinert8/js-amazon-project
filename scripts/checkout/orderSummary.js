969119eacc35903b8daf41edad9153ee62814806
bmeinert8 <brian.meinert1@gmail.com>
2025-01-15 09:47:45 -0500

    Refactor product handling to use Product class and update price formatting

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -58,7 +58,7 @@ export function renderOrderSummary() {
               ${matchingProduct.name}
             </div>
             <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
+              ${matchingProduct.getPrice()}
             </div>
             <div class="product-quantity">
               <span class="quantity-label">
