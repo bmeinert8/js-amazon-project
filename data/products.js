2f99f13c5d15dfcc7bce73a0d03cffe0dcb41d1b
bmeinert8 <brian.meinert1@gmail.com>
2025-01-16 14:36:15 -0500

    convert load projects from xml request to fetch

--- data/products.js
+++ data/products.js
@@ -61,6 +61,30 @@ class Clothing extends Product{
 //Load the products from the backend
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+    ).then((response) => {
+    return response.json();
+  }). then((productData) => {
+    products = productData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
