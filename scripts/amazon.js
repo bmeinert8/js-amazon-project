6b9cad9c50d6852487b4a5a12064579cd2b28982
bmeinert8 <brian.meinert1@gmail.com>
2025-01-17 10:29:27 -0500

    add search functionality to product listing page with URL parameter filtering

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -15,18 +15,38 @@ function renderProductsGrid() {
   let productsHTML = '';
 
   //loop through the products array adding each product to the empty productsHTML variable string.
-  products.forEach( (product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if (keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+
+      return matchingKeyword ||
+        product.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
       <div class="product-container">
         <div class="product-image-container">
           <img class="product-image"
             src="${product.image}">
         </div>
-
         <div class="product-name limit-text-to-2-lines">
           ${product.name}
         </div>
-
         <div class="product-rating-container">
           <img class="product-rating-stars"
             src="${product.getStarsUrl()}">
@@ -34,13 +54,11 @@ function renderProductsGrid() {
             ${product.rating.count}
           </div>
         </div>
-
         <div class="product-price">
           ${product.getPrice()}
         </div>
-        
         <div class="product-quantity-container">
-          <select class="js-quantity-selector-${product.id}">
+          <select>
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -53,21 +71,18 @@ function renderProductsGrid() {
             <option value="10">10</option>
           </select>
         </div>
-
         ${product.extraInfoHTML()}
-          
         <div class="product-spacer"></div>
-
-        <div class="added-to-cart js-added-to-cart-${product.id}">
+        <div class="added-to-cart">
           <img src="images/icons/checkmark.png">
           Added
         </div>
-
         <button class="add-to-cart-button button-primary js-add-to-cart"
         data-product-id="${product.id}">
           Add to Cart
         </button>
-      </div>`;
+      </div>
+    `;
   });
 
   //add the generated html to the products-grid element in the DOM
@@ -125,5 +140,10 @@ function renderProductsGrid() {
         updateCartQuantity();
         timeoutMessage(productId);
       });
+    });
+    document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
     });
 }
